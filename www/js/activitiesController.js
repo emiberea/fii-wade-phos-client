7bb12dfd17cc7533fe9c5035b45a181cc513e26f
Simona Serseniuc <simona.serseniuc@gmail.com>
2016-01-28 15:33:49 +0200

    added modals, connection to blog

--- www/js/activitiesController.js
+++ www/js/activitiesController.js
@@ -1,11 +1,21 @@
 app.controller("activitiesController", function ($scope, $rootScope, $ionicModal) {
   console.log("activities contoller");
+
   $ionicModal.fromTemplateUrl('../views/web/newActivityModal.html', function($ionicModal) {
-       $scope.modal = $ionicModal;
+       $scope.newActivityModal = $ionicModal;
    }, {
        // Use our scope for the scope of the modal to keep it simple
        scope: $scope,
        // The animation we want to use for the modal entrance
        animation: 'slide-in-up'
    });
+
+   $ionicModal.fromTemplateUrl('../views/web/sugestionModal.html', function($ionicModal) {
+        $scope.sugestionModal = $ionicModal;
+    }, {
+        // Use our scope for the scope of the modal to keep it simple
+        scope: $scope,
+        // The animation we want to use for the modal entrance
+        animation: 'slide-in-up'
+    });
 });
--- www/js/personsController.js
+++ www/js/personsController.js
@@ -1,3 +1,21 @@
-app.controller("personsController", function ($scope, $rootScope) {
+app.controller("personsController", function ($scope, $rootScope, $ionicModal) {
   console.log("persons contoller");
+
+  $ionicModal.fromTemplateUrl('../views/web/newPersonModal.html', function($ionicModal) {
+       $scope.newPersonModal = $ionicModal;
+   }, {
+       // Use our scope for the scope of the modal to keep it simple
+       scope: $scope,
+       // The animation we want to use for the modal entrance
+       animation: 'slide-in-up'
+   });
+
+   $ionicModal.fromTemplateUrl('../views/web/sugestionModal.html', function($ionicModal) {
+        $scope.sugestionModal = $ionicModal;
+    }, {
+        // Use our scope for the scope of the modal to keep it simple
+        scope: $scope,
+        // The animation we want to use for the modal entrance
+        animation: 'slide-in-up'
+    });
 });
